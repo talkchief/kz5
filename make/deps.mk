e7409bc0ec70ec6a31e734bbbc1e071541092ce8
lazedo <luis.azedo@factorlusitano.com>
2020-06-19 14:33:53 +0000

    update ra to v1.1.2

--- make/deps.mk
+++ make/deps.mk
@@ -133,7 +133,7 @@ dep_proper = git https://github.com/2600hz/erlang-proper v1.3
 
 dep_recon = git https://github.com/2600hz/erlang-recon 2.4.0
 
-dep_ra = git https://github.com/2600hz/erlang-ra.git v1.0.4
+dep_ra = git https://github.com/2600hz/erlang-ra.git v1.1.2
 
 dep_ranch = git https://github.com/2600hz/erlang-ranch 1.7.1
 
