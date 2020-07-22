7c50d7ba47922c75d5659a6faa37416d4d7b7f44
icehess <icehess@users.noreply.github.com>
2020-07-22 10:50:14 -0700

    use base branch as branch name when fetch-apps (#20)

--- make/app_urls.mk
+++ make/app_urls.mk
@@ -3,34 +3,37 @@
 
 FETCH_AS ?= https://github.com/
 
+BASE_BRANCH ?= origin/master
+BRANCH = $(subst origin/,,$(BASE_BRANCH))
 
-dep_blackhole = git $(FETCH_AS)2600hz/kazoo-blackhole.git master
-dep_braintree = git $(FETCH_AS)2600hz/kazoo-braintree.git master
-dep_call_inspector = git $(FETCH_AS)2600hz/kazoo-call-inspector.git master
-dep_callflow = git $(FETCH_AS)2600hz/kazoo-callflow.git master
-dep_cdr = git $(FETCH_AS)2600hz/kazoo-cdr.git master
-dep_conference = git $(FETCH_AS)2600hz/kazoo-conference.git master
-dep_crossbar = git $(FETCH_AS)2600hz/kazoo-crossbar.git master
-dep_doodle = git $(FETCH_AS)2600hz/kazoo-doodle.git master
-dep_ecallmgr = git $(FETCH_AS)2600hz/kazoo-ecallmgr.git master
-dep_fax = git $(FETCH_AS)2600hz/kazoo-fax.git master
-dep_hangups = git $(FETCH_AS)2600hz/kazoo-hangups.git master
-dep_hotornot = git $(FETCH_AS)2600hz/kazoo-hotornot.git master
-dep_jonny5 = git $(FETCH_AS)2600hz/kazoo-jonny5.git master
-dep_media_mgr = git $(FETCH_AS)2600hz/kazoo-media-mgr.git master
-dep_milliwatt = git $(FETCH_AS)2600hz/kazoo-milliwatt.git master
-dep_omnipresence = git $(FETCH_AS)2600hz/kazoo-omnipresence.git master
-dep_pivot = git $(FETCH_AS)2600hz/kazoo-pivot.git master
-dep_pusher = git $(FETCH_AS)2600hz/kazoo-pusher.git master
-dep_registrar = git $(FETCH_AS)2600hz/kazoo-registrar.git master
-dep_reorder = git $(FETCH_AS)2600hz/kazoo-reorder.git master
-dep_skel = git $(FETCH_AS)2600hz/kazoo-skel.git master
-dep_stats = git $(FETCH_AS)2600hz/kazoo-stats.git master
-dep_stepswitch = git $(FETCH_AS)2600hz/kazoo-stepswitch.git master
-dep_sysconf = git $(FETCH_AS)2600hz/kazoo-sysconf.git master
-dep_tasks = git $(FETCH_AS)2600hz/kazoo-tasks.git master
-dep_teletype = git $(FETCH_AS)2600hz/kazoo-teletype.git master
-dep_trunkstore = git $(FETCH_AS)2600hz/kazoo-trunkstore.git master
-dep_webhooks = git $(FETCH_AS)2600hz/kazoo-webhooks.git master
 
-dep_kazoo_ast = git $(FETCH_AS)2600hz/kazoo-ast.git master
+dep_blackhole = git $(FETCH_AS)2600hz/kazoo-blackhole.git $(BRANCH)
+dep_braintree = git $(FETCH_AS)2600hz/kazoo-braintree.git $(BRANCH)
+dep_call_inspector = git $(FETCH_AS)2600hz/kazoo-call-inspector.git $(BRANCH)
+dep_callflow = git $(FETCH_AS)2600hz/kazoo-callflow.git $(BRANCH)
+dep_cdr = git $(FETCH_AS)2600hz/kazoo-cdr.git $(BRANCH)
+dep_conference = git $(FETCH_AS)2600hz/kazoo-conference.git $(BRANCH)
+dep_crossbar = git $(FETCH_AS)2600hz/kazoo-crossbar.git $(BRANCH)
+dep_doodle = git $(FETCH_AS)2600hz/kazoo-doodle.git $(BRANCH)
+dep_ecallmgr = git $(FETCH_AS)2600hz/kazoo-ecallmgr.git $(BRANCH)
+dep_fax = git $(FETCH_AS)2600hz/kazoo-fax.git $(BRANCH)
+dep_hangups = git $(FETCH_AS)2600hz/kazoo-hangups.git $(BRANCH)
+dep_hotornot = git $(FETCH_AS)2600hz/kazoo-hotornot.git $(BRANCH)
+dep_jonny5 = git $(FETCH_AS)2600hz/kazoo-jonny5.git $(BRANCH)
+dep_media_mgr = git $(FETCH_AS)2600hz/kazoo-media-mgr.git $(BRANCH)
+dep_milliwatt = git $(FETCH_AS)2600hz/kazoo-milliwatt.git $(BRANCH)
+dep_omnipresence = git $(FETCH_AS)2600hz/kazoo-omnipresence.git $(BRANCH)
+dep_pivot = git $(FETCH_AS)2600hz/kazoo-pivot.git $(BRANCH)
+dep_pusher = git $(FETCH_AS)2600hz/kazoo-pusher.git $(BRANCH)
+dep_registrar = git $(FETCH_AS)2600hz/kazoo-registrar.git $(BRANCH)
+dep_reorder = git $(FETCH_AS)2600hz/kazoo-reorder.git $(BRANCH)
+dep_skel = git $(FETCH_AS)2600hz/kazoo-skel.git $(BRANCH)
+dep_stats = git $(FETCH_AS)2600hz/kazoo-stats.git $(BRANCH)
+dep_stepswitch = git $(FETCH_AS)2600hz/kazoo-stepswitch.git $(BRANCH)
+dep_sysconf = git $(FETCH_AS)2600hz/kazoo-sysconf.git $(BRANCH)
+dep_tasks = git $(FETCH_AS)2600hz/kazoo-tasks.git $(BRANCH)
+dep_teletype = git $(FETCH_AS)2600hz/kazoo-teletype.git $(BRANCH)
+dep_trunkstore = git $(FETCH_AS)2600hz/kazoo-trunkstore.git $(BRANCH)
+dep_webhooks = git $(FETCH_AS)2600hz/kazoo-webhooks.git $(BRANCH)
+
+dep_kazoo_ast = git $(FETCH_AS)2600hz/kazoo-ast.git $(BRANCH)
