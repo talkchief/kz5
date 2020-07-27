219626eeced26ad0630e859925a918e7892cc186
lazedo <luis.azedo@factorlusitano.com>
2020-07-27 10:20:08 +0100

    add meta dep

--- make/deps.mk
+++ make/deps.mk
@@ -20,6 +20,7 @@ DEPS ?= amqp_client \
 	lager \
 	lager_syslog \
 	meck \
+	meta \
 	nklib \
 	plists \
 	poolboy \
@@ -176,3 +177,6 @@ dep_hep = git https://github.com/2600hz/hep-erlang 5f18e91e45d49d3d7013fc93897d5
 # Homer encapsulation protocol
 # merged lazedo/hep changes
 # added specs for dialyzer happiness
+
+# runtime code generation
+dep_meta = git https://github.com/2600hz/erlang-meta 0.1.3
