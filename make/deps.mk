99f0fae5f1ae021f209cd113da6b7a2011759f85
Hesaam Farhang <hesaam@2600hz.com>
2020-10-12 16:49:34 -0700

    updating apns to the latest

--- make/deps.mk
+++ make/deps.mk
@@ -167,7 +167,7 @@ dep_gun = git https://github.com/ninenines/gun 2.0.0-pre.2
 dep_folsom = git https://github.com/2600hz/erlang-folsom 0.8.2
 # used by hangups
 
-dep_apns = git https://github.com/2600hz/erlang-apns4erl.git aba1fa96a4abbbb2c1628ad5d604f482aad4d12f # latest commit SHA to 2600hz branch
+dep_apns = git https://github.com/2600hz/erlang-apns4erl.git 20668a1f5fde5a7afd195d03a5cd30825786f2c5 # latest commit SHA to 2600hz branch
 
 dep_fcm = git https://github.com/2600hz/erlang-fcm.git b2f68a4c6f0f59475597a35e2dc9be13d9ba2910
 # Firebase cloud messaging
