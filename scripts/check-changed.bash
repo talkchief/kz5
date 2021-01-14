6c42288f8f5f6a8ddf96c877d762ddaa24110e16
Hesaam Farhang <hesaam@2600hz.com>
2021-01-14 13:22:25 -0800

    only print debug info if it is debug run (#43)

--- scripts/check-changed.bash
+++ scripts/check-changed.bash
@@ -8,14 +8,23 @@ pushd "$(dirname "$0")" > /dev/null
 
 ROOT=$(readlink -f "$(pwd -P)"/..)
 
-echo "Checking for any changed files" >&2
-echo "ROOT=${ROOT}" >&2
-echo >&2
+_ci_debug() {
+    msg="$1"
+    if [ -n "${DEBUG}" ]; then
+        echo "${msg}" >&2
+    elif [ -n "${CIRCLECI}" ]; then
+        echo "${msg}" >&2
+    fi
+}
+
+_ci_debug "Checking for any changed files"
+_ci_debug "ROOT=${ROOT}"
+_ci_debug
 
 function get_changed {
     base_branch=$(<$ROOT/.base_branch)
     [ -f $1/.base_branch ] && base_branch=$(<$1/.base_branch);
-    echo "- checking $1 (base_branch=${base_branch})" >&2
+    _ci_debug "- checking $1 (base_branch=${base_branch})"
     diff=""
     for file in $(git -C $1 --no-pager diff --name-only HEAD $base_branch); do
         diff+=" $1/$file"
