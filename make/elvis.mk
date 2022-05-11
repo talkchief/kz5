67ee0f8697e9ca7a734ce438de103cf3db624789
James Aimonetti <james@2600hz.com>
2022-05-11 09:58:02 -0700

    Cleanup elvis when asked

--- make/elvis.mk
+++ make/elvis.mk
@@ -8,13 +8,13 @@ ELVIS = $(ROOT)/elvis
 
 FETCH_AS ?= https://github.com/
 
-$(ELVIS_DEP_DIR): $(DEPS_HASH_FILE)
+$(ELVIS_DEP_DIR):
 	@echo creating elvis dir $(ELVIS_DEP_DIR)
-	@mkdir -p $(ELVIS_DEP_DIR)
+	@mkdir -p $(shell dirname $(ELVIS_DEP_DIR))
 	ROOT=$(ROOT) DEPS_MK="$(ROOT)/make/elvis.mk" $(MAKE) -C $(DEPS_DIR)/ all
 
 .PHONY: elvis-escript
-elvis-escript:
+elvis-escript: $(ELVIS_DEP_DIR)
 	@ERLANG_MK_FILENAME=$(ROOT)/erlang.mk DEPS_DIR=$(ROOT)/deps ESCRIPT_NAME=elvis ESCRIPT_FILE=$(ROOT)/elvis make -C $(ELVIS_DEP_DIR) escript
 
 $(ELVIS): $(ELVIS_DEP_DIR)
@@ -23,4 +23,9 @@ $(ELVIS): $(ELVIS_DEP_DIR)
 .PHONY: elvis
 elvis: $(ELVIS)
 # need $(CHANGED_ERL) absolute paths to be relative to $(ROOT)
-	@ERL_LIBS=$(DEPS_DIR):$(CORE_DIR) $(ELVIS) --config $(ROOT)/make/elvis.config --verbose -k --parallel auto rock $(subst $(ROOT)/,,$(CHANGED_ERL))
+	@ERL_LIBS=$(DEPS_DIR):$(CORE_DIR) $(ELVIS) --config $(ROOT)/make/elvis.config --verbose -k --parallel auto rock $(subst $(ROOT)/,,$(filter %.erl,$(CHANGED_ERL)))
+
+.PHONY: clean-elvis
+clean-elvis:
+	@rm -f $(ELVIS)
+	@rm -rf $(ELVIS_DEP_DIR)
