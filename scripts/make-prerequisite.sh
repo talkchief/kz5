f5d8c43a431498673024ae9f34e97839084d0640
James Aimonetti <james@2600hz.com>
2023-05-16 10:12:04 -0700

    install make4 when running in CI

--- scripts/make-prerequisite.sh
+++ scripts/make-prerequisite.sh
@@ -15,3 +15,33 @@ else
     echo "git is missing"
     exit 1
 fi
+
+MAKE_MAJOR_VERSION=4
+INSTALL_VERSION=4.3
+if type make > /dev/null 2>&1; then
+    MakeMajorVersion=`make -v | head -n1 | awk '{print $3}' | awk -F'.' '{print $1}'`
+
+    if [ $MakeMajorVersion -ne $MAKE_MAJOR_VERSION ]
+    then
+        if [ -n "${CIRCLECI}" ]; then
+            mkdir tmp/
+            pushd tmp
+            wget http://ftp.gnu.org/gnu/make/make-${INSTALL_VERSION}.tar.gz
+            tar xvf make-${INSTALL_VERSION}.tar.gz
+            cd make-${INSTALL_VERSION}/
+            ./configure
+            make
+            sudo make install
+            popd
+            sudo rm -rf tmp/
+            echo "installed make ${INSTALL_VERSION}"
+            exit 0
+        else
+            echo "make major version ${MAKE_MAJOR_VERSION} is required."
+            exit 1
+        fi
+    fi
+else
+    echo "make is missing"
+    exit 1
+fi
