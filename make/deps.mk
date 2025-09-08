999c9659f8cde7db209b3131fcc427aa46d7f72b
Luis Azedo <luis.azedo@ooma.com>
2025-09-08 09:48:17 +0000

    KZOO-686 update amqp_dist dependency
    * auto_connect delay based on downtime/uptime
    * connection labels (zones)
    * publish/connect on filtered labels
    
    See merge request oomaforbin/oomacorp/2600hz/kazoo5!5
    
    (cherry picked from commit d2ab8e99cb7db83c2326b623089e3ffe807b9830)

--- make/deps.mk
+++ make/deps.mk
@@ -57,7 +57,7 @@ endif
 
 dep_amqp_client = hex 3.12.13
 
-dep_amqp_dist = git https://github.com/2600hz/erlang-amqp_dist.git 581d4b53e1fb3e8247ad856bef106015304d1906
+dep_amqp_dist = git https://github.com/2600hz/erlang-amqp_dist.git 712e5254e2c4a084e2235563d79a1452aea7c283
 
 dep_esaml = git https://github.com/2600hz/erlang-esaml.git 9fe06697234113eb1a64a009a6a1167ba72c9c04
 # priv app usage, branch 2600Hz-otp-26
