4b1f117f37af2ffd6aaeb6fdc6a2fa652fa6dc88
James Aimonetti <james@2600hz.com>
2026-03-10 10:01:27 -0700

    Update elvis in deps.mk too

--- make/deps.mk
+++ make/deps.mk
@@ -225,4 +225,4 @@ dep_qrcode = git https://github.com/2600hz/erlang-qrcode 7faa72913a4f8267c10d8f4
 dep_qdate = git https://github.com/2600hz/erlang-qdate 2072b49220dc0cfad59f1163119e1e76e55240a1
 dep_qdate_localtime = git https://github.com/2600hz/erlang-qdate_localtime cee705be45df8bcdcc7f77d371d4b34ef52d369b
 
-dep_elvis = git https://github.com/2600hz/erlang-elvis 2600Hz
+dep_elvis = git https://github.com/2600hz/erlang-elvis 26ef5f2
